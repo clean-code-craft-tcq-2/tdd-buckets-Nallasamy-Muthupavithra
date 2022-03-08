5fe6345cb6be5dc2df5e5d5d5f2ebb879ce5f72a
Nallasamy-Muthupavithra <90901613+Nallasamy-Muthupavithra@users.noreply.github.com>
2022-03-08 22:03:25 +0530

    Update TestDrivenRange.java

--- src/main/java/TestDrivenRange/TestDrivenRange.java
+++ src/main/java/TestDrivenRange/TestDrivenRange.java
@@ -2,7 +2,7 @@ package TestDrivenRange;
 
 public class TestDrivenRange{
 
-public String findNoOfReadingsInRange(String currentSamples){
+public Static String findNoOfReadingsInRange(String currentSamples){
   return "4-5,2";
 }
   
