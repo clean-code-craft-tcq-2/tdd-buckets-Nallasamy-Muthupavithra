d06ad61361b7c58582e726f372d35eaa63793e56
Nallasamy-Muthupavithra <90901613+Nallasamy-Muthupavithra@users.noreply.github.com>
2022-03-15 20:17:21 +0530

    Update TestDrivenRangeTest.java

--- src/test/java/TestDrivenRange/TestDrivenRangeTest.java
+++ src/test/java/TestDrivenRange/TestDrivenRangeTest.java
@@ -1,16 +1,23 @@
 package TestDrivenRange;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
 
 import TestDrivenRange.TestDrivenRange;
 
-public class TestDrivenRangeTest{
+import org.junit.Test;
 
-@Test
-public void testRange(){ 
-   assertTrue(TestDrivenRange.findNoOfReadingsInRange("4,5")== "4-5,2");
+public class TestDrivenRangeTest {
+
+  @Test
+  public void testRange() {
+    assertNull(TestDrivenRange.findNoOfReadingsInRange());
+    assertNull(TestDrivenRange.findNoOfReadingsInRange(4));
+    assertNotEquals("4-5,3", TestDrivenRange.findNoOfReadingsInRange(4, 5)[0]);
+    assertEquals("4-5,2", TestDrivenRange.findNoOfReadingsInRange(4, 5)[0]);
+    assertEquals("4-7,4", TestDrivenRange.findNoOfReadingsInRange(4, 5, 6, 7)[0]);
+  }
+
 }
-   
-}
+
