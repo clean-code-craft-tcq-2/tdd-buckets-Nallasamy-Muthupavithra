c284c6b0d06de4c08d696a65fe696e43ddef3b44
Nallasamy-Muthupavithra <90901613+Nallasamy-Muthupavithra@users.noreply.github.com>
2022-03-16 09:47:46 +0530

    Update TestDrivenRangeTest.java

--- src/test/java/TestDrivenRange/TestDrivenRangeTest.java
+++ src/test/java/TestDrivenRange/TestDrivenRangeTest.java
@@ -14,10 +14,20 @@ public class TestDrivenRangeTest {
   public void testRange() {
     assertNull(TestDrivenRange.findNoOfReadingsInRange());
     assertNull(TestDrivenRange.findNoOfReadingsInRange(4));
-    assertNotEquals("4-5,3", TestDrivenRange.findNoOfReadingsInRange(4, 5)[0]);
-    assertEquals("4-5,2", TestDrivenRange.findNoOfReadingsInRange(4, 5)[0]);
-    assertEquals("4-7,4", TestDrivenRange.findNoOfReadingsInRange(4, 5, 6, 7)[0]);
+    assertNotEquals("4-5,3", TestDrivenRange.findNoOfReadingsInRange(4, 5).get(0));
+    assertEquals("4-5,2", TestDrivenRange.findNoOfReadingsInRange(4, 5).get(0));
+    assertEquals("4-7,4", TestDrivenRange.findNoOfReadingsInRange(4, 5, 6, 7).get(0));
+
+    List<String> actualOutput = TestDrivenRange.findNoOfReadingsInRange(3, 3, 5, 4, 10, 11, 12);
+    assertEquals("3-5,4", actualOutput.get(0));
+    assertEquals("10-12,3", actualOutput.get(1));
+
+    List<String> actualOutput1 = TestDrivenRange.findNoOfReadingsInRange(5, 4, 10, 11, 12);
+    assertEquals("4-5,2", actualOutput1.get(0));
+    assertEquals("10-12,3", actualOutput1.get(1));
+
+    List<String> actualOutput2 = TestDrivenRange.findNoOfReadingsInRange(4, 10, 11, 12);
+    assertEquals("10-12,3", actualOutput2.get(0));
   }
 
 }
-
