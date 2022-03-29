ada76a5bff7e56a44f8c83d839338845800d77db
Nallasamy-Muthupavithra <90901613+Nallasamy-Muthupavithra@users.noreply.github.com>
2022-03-29 21:34:44 +0530

    Extension 1 integration

--- src/test/java/TestDrivenRange/TestDrivenRangeTest.java
+++ src/test/java/TestDrivenRange/TestDrivenRangeTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 
 public class TestDrivenRangeTest {
 
-   @Test
+  @Test
   public void testRange() {
     assertNull(TestDrivenRange.findNoOfReadingsInRange());
     assertNull(TestDrivenRange.findNoOfReadingsInRange(4));
@@ -20,31 +20,40 @@ public class TestDrivenRangeTest {
     assertEquals("4-5,2", TestDrivenRange.findNoOfReadingsInRange(4, 5).get(0));
     assertEquals("4-7,4", TestDrivenRange.findNoOfReadingsInRange(4, 5, 6, 7).get(0));
 
-    List<String> actualOutput = TestDrivenRange.findNoOfReadingsInRange(3, 3, 5, 4, 10, 11, 12);
+    List<String> actualOutput = TestDrivenRange.findNoOfReadingsInRange(3, 3, 5, 4, 7, 8, 9);
     assertEquals("3-5,4", actualOutput.get(0));
-    assertEquals("10-12,3", actualOutput.get(1));
+    assertEquals("7-9,3", actualOutput.get(1));
 
-    List<String> actualOutput1 = TestDrivenRange.findNoOfReadingsInRange(5, 4, 10, 11, 12);
+    List<String> actualOutput1 = TestDrivenRange.findNoOfReadingsInRange(5, 4, 7, 8, 9);
     assertEquals("4-5,2", actualOutput1.get(0));
-    assertEquals("10-12,3", actualOutput1.get(1));
+    assertEquals("7-9,3", actualOutput1.get(1));
 
-    List<String> actualOutput2 = TestDrivenRange.findNoOfReadingsInRange(4, 10, 11, 12);
-    assertEquals("10-12,3", actualOutput2.get(0));
+    List<String> actualOutput2 = TestDrivenRange.findNoOfReadingsInRange(4, 7, 8, 9);
+    assertEquals("7-9,3", actualOutput2.get(0));
+  }
+
+  @Test
+  public void testFindRangesForA2DInpt() {
+    List<String> actualOutput = TestDrivenRange.findRangesForA2DInput(1228, 1228, 2047, 1637, 2865, 3275, 3684);
+    assertEquals("3-5,4", actualOutput.get(0));
+    assertEquals("7-9,3", actualOutput.get(1));
   }
 
   @Test
   public void testConvertA2DListIntoAmps() {
-    List<Integer> convertA2DInputToAmps = TestDrivenRange.convertA2DListIntoAmps(4092, 4503, 1146);
-    assert (10 == convertA2DInputToAmps.get(0));
-    assert (11 == convertA2DInputToAmps.get(1));
-    assert (3 == convertA2DInputToAmps.get(2));
+    int[] sampleInAmps = TestDrivenRange.convertA2DListIntoAmps(4092, 4503, 1146);
+    assert (10 == sampleInAmps[0]);
+    assert (3 == sampleInAmps[1]);
+
+    int[] sampleInAmps1 = TestDrivenRange.convertA2DListIntoAmps();
+    assert (0 == sampleInAmps1.length);
   }
 
   @Test
   public void testConvertA2DInputToAmps() {
     assertEquals(10, TestDrivenRange.convertA2DInputToAmps(4092));
     assertEquals(10, TestDrivenRange.convertA2DInputToAmps(4094));
-    assertEquals(11, TestDrivenRange.convertA2DInputToAmps(4503));
+    assertEquals(9, TestDrivenRange.convertA2DInputToAmps(3800));
   }
 
   @Test
